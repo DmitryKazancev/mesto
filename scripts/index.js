4ce521cccfa59d78025ebd570356ef6b67d8f318
Dmitrii Kazantsev <dmitrykaz@mail.ru>
2023-03-14 00:51:13 +0500

    feat: create six card

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,33 @@ let jobAuthor = document.querySelector('.profile__info-description');
 let nameInput = document.querySelector('.popup__input_type_name');
 let jobInput = document.querySelector('.popup__input_type_job');
 
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ]; 
+
 //Open popup window function
 function openPopup () {
     popupWindow.classList.add('popup_opened');
@@ -29,7 +56,21 @@ function handleFormSubmit (evt) {
     closePopup();
 };
 
+
+
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit); 
+
+//add cards 
+initialCards.forEach((card) => {
+    const elementsSection = document.querySelector('.elements');
+    const elementTemplate = document.querySelector('#element').content;
+    const elementCard = elementTemplate.querySelector('.element').cloneNode(true);
+
+    elementCard.querySelector('.element__image').src = card.link;
+    elementCard.querySelector('.element__title').textContent = card.name;
+    elementsSection.append(elementCard);
+});
+
 
